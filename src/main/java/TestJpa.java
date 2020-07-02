fe2438188b0108e3df1812cf55ff6766ac922fdb
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-02 12:03:01 +0200

    TP01 JPA

---
+++ src/main/java/TestJpa.java
@@ -0,0 +1,13 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+		EntityManager em = entityManagerFactory.createEntityManager();
+
+	}
+
+}
